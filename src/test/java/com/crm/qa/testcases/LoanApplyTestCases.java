568f34788fcb0e6c833ce1d0260815f3cdd4d62d
KamalQA10 <qa10engineer@gmail.com>
2022-11-04 18:15:46 +0530

    hhhhh

--- src/test/java/com/crm/qa/testcases/LoanApplyTestCases.java
+++ src/test/java/com/crm/qa/testcases/LoanApplyTestCases.java
@@ -10,7 +10,7 @@ import CommonAssertions.Assertions;
 
 @Test
 public class LoanApplyTestCases extends TestBase {
-//kkkkkkk
+//kkkkkkkk
 		ApplyPage applypage;
 		SelectAnOfferPage selectAnOfferPage;
 
